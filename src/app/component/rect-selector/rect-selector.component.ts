eef27ceda7775aa9db823fc87ced179e499b1904
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2024-07-14 22:23:47 +0200

    refactor rect-selector (add comments)

--- src/app/component/rect-selector/rect-selector.component.ts
+++ src/app/component/rect-selector/rect-selector.component.ts
@@ -55,18 +55,22 @@ export class RectSelectorComponent implements OnInit {
       evt.preventDefault()
       evt.stopPropagation()
       // The SVG specification says that if width or height are negative then the rectangle is not drawn.
-      // negative value causes errors
+      // negative value causes errors. Solution is to add start X & Y variables and commute with shape.x & y
       if (evt.offsetX >= this.startX) {
+        // mouse position.x is right
         this.shape.x = this.startX
         this.shape.w = evt.offsetX - this.startX;
       } else {
+        // now mouse position.x is left
         this.shape.x = evt.offsetX
         this.shape.w = this.startX - evt.offsetX;
       }
       if (evt.offsetY >= this.startY) {
+        // mouse position.y is bottom
         this.shape.y = this.startY
         this.shape.h = evt.offsetY - this.startY;
       } else {
+        // now mouse position.y is top
         this.shape.y = evt.offsetY
         this.shape.h = this.startY - evt.offsetY;
       }
