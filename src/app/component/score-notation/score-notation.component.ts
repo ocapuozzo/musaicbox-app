ae735e8e868caaa4757eb374062e5fa96ba3d7ac
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2024-12-04 17:56:47 +0100

    (PCS page) update abcjs

--- src/app/component/score-notation/score-notation.component.ts
+++ src/app/component/score-notation/score-notation.component.ts
@@ -1,8 +1,8 @@
 import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {IPcs} from "../../core/IPcs";
-import * as abcjs from "abcjs";
 import {StringHash} from "../../utils/StringHash";
 import {ScoreDrawingAbcNotation} from "../../ui/ScoreDrawingAbcNotation";
+import abcjs from "abcjs";
 
 @Component({
   selector: 'app-score-notation',
@@ -43,29 +43,92 @@ export class ScoreNotationComponent {
     const len = this.containerCanvas.nativeElement.clientWidth
     const suffix = 'X:1\nL: 1/4\nK:C\n';
     const codeAbc = suffix + ScoreDrawingAbcNotation.fromPcsToABCNotation(this.pcs)
-    const visualObj = abcjs.renderAbc(
-      "paper-" + this.randomId,
-      codeAbc,
-      {
-        //scale: .9,
-        staffwidth: len,
-        paddingleft: 0,
-        paddingright: 10,
-        responsive: "resize"
-      })[0];
-    if (abcjs.synth.supportsAudio()) {
-      // for css integration, see angular.json (architect/build/options/styles)
-      let synthControl = new abcjs.synth.SynthController();
-      synthControl.load("#abc-audio-" + this.randomId, null, {displayRestart: false, displayPlay: true, displayProgress: true});
-      synthControl.setTune(visualObj, false,  {
-        defaultQpm: 280, // no change...
-        qpm: 280, // no change...
-        onEnded: () => {},
-      } )
-    } else {
-      const id= `abc-audio-${this.randomId}`
-      document.querySelector(`#${id}`)!.innerHTML = "<div class='audio-error'>Audio is not supported in this browser.</div>"
+
+    class CursorControl  {
+      beatSubdivisions = 2;
+      onStart = function() {
+        // console.log("The tune has started playing.");
+      }
+      onFinished = function() {
+        // console.log("The tune has stopped playing.");
+      }
+      onBeat = function(beatNumber : any) {
+        // console.log("Beat " + beatNumber + " is happening.");
+      }
+      onEvent = function(event : any) {
+        // a note is playing
+        // console.log("An event is happening", event);
+      }
     }
+    const cursorControl = new CursorControl();
+    let abcOptions = { add_classes: true };
+    let audioParams = { chordsOff: true, options: {qpm:290} };
+
+    if (abcjs.synth.supportsAudio()) {
+      let synthControl = new abcjs.synth.SynthController();
+      synthControl.load("#abc-audio-" + this.randomId,
+        cursorControl,
+        {
+          // displayLoop: true,
+          displayRestart: false,
+          displayPlay: true,
+          displayProgress: true,
+          // displayWarp: true
+        }
+      );
+
+      const visualObj = abcjs.renderAbc(
+        "paper-" + this.randomId,
+        codeAbc,
+        {
+          //scale: .9,
+          staffwidth: len,
+          paddingleft: 0,
+          paddingright: 10,
+          responsive: "resize"
+        })[0];
+      // var visualObj = ABCJS.renderAbc("paper", abc, abcOptions);
+
+      let createSynth = new abcjs.synth.CreateSynth();
+      createSynth.init({ visualObj: visualObj }).then(function () {
+        synthControl.setTune(visualObj, false, audioParams).then(function () {
+          console.log("Audio successfully loaded.")
+        }).catch(function (error) {
+          console.warn("Audio problem:", error);
+        });
+      }).catch(function (error) {
+        console.warn("Audio problem:", error);
+      });
+    } else {
+        const id= `abc-audio-${this.randomId}`
+        document.querySelector(`#${id}`)!.innerHTML = "<div class='audio-error'>Audio is not supported in this browser.</div>"
+    }
+
+
+    //
+    // const visualObj = abcjs.renderAbc(
+    //   "paper-" + this.randomId,
+    //   codeAbc,
+    //   {
+    //     //scale: .9,
+    //     staffwidth: len,
+    //     paddingleft: 0,
+    //     paddingright: 10,
+    //     responsive: "resize"
+    //   })[0];
+    // if (abcjs.synth.supportsAudio()) {
+    //   // for css integration, see angular.json (architect/build/options/styles)
+    //   let synthControl = new abcjs.synth.SynthController();
+    //   synthControl.load("#abc-audio-" + this.randomId, null, { displayRestart: false, displayPlay: true, displayProgress: true});
+    //   synthControl.setTune(visualObj, false,  {
+    //     defaultQpm: 280, // don't work...
+    //     qpm: 280, // don't work...
+    //     onEnded: () => {},
+    //   } )
+    // } else {
+    //   const id= `abc-audio-${this.randomId}`
+    //   document.querySelector(`#${id}`)!.innerHTML = "<div class='audio-error'>Audio is not supported in this browser.</div>"
+    // }
   }
 
 }
