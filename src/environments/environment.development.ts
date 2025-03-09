f35de27000ac4371b663dcdcc8223d15041ad226
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-03-09 20:35:43 +0100

    (analyse) problem with some images that do not render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"2025-03-09 19:21"
+  buildVersion:"2025-03-09 20:34"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"2025-03-09 19:21"
+  buildVersion:"2025-03-09 20:34"
 };
