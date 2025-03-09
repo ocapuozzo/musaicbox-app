656e88b458937a2aaf8332c1a28ea8dac0dd99c3
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-03-09 19:21:51 +0100

    (analyse) somme image do not render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"2025-03-09 12:26"
+  buildVersion:"2025-03-09 19:21"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"2025-03-09 12:26"
+  buildVersion:"2025-03-09 19:21"
 };
