779167bc6ebd8069aa304ca6e76e63a07082d581
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-03-10 14:37:35 +0100

    change angular.json font -> inline -> false

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-date_heure=$(date +"%Y-%m-%d %H:%M")
-sed -i "s/buildVersion:.*/buildVersion:\"$date_heure\"/" ./src/environments/environment.ts
-sed -i "s/buildVersion:.*/buildVersion:\"$date_heure\"/" ./src/environments/environment.development.ts
+date_time=$(date +"%Y-%m-%d %H:%M")
+sed -i "s/buildVersion:.*/buildVersion:\"$date_time\"/" ./src/environments/environment.ts
+sed -i "s/buildVersion:.*/buildVersion:\"$date_time\"/" ./src/environments/environment.development.ts
 ng build
 echo "cp ./docs/index.html ./docs/404.html"
 cp ./docs/index.html ./docs/404.html
 
+## generate documentation
 # cd analyse/documentation || exit
 # asciidoctor ./Musaicbox.adoc
 # cd ../..
 
 echo "cp -r ./analyse/documentation/. ./docs"
-# doc can be consulted via About link (with CTRL key pressed)
-# must be generated before
+# doc can be consulted via About link (with CTRL key pressed, while waiting for the analysis to be more advanced.)
 cp -r ./analyse/documentation/. ./docs
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"2025-03-09 20:46"
+  buildVersion:"2025-03-10 14:35"
 };
