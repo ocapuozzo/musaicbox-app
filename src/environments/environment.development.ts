a518f954ee94885f3147f40cbda076d541bd8e40
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-03-20 18:26:19 +0100

    (analysis/doc) section "zero fixed problem" done.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"0.1.2--2025-03-20--16-09"
+  buildVersion:"0.1.2--2025-03-20--18-24"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"0.1.2--2025-03-20--16-09"
+  buildVersion:"0.1.2--2025-03-20--18-24"
 };
