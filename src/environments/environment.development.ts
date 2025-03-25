c5e81fb7ddaf81c277116c75c16c3776d1a6e56f
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-03-25 10:07:16 +0100

    (analysis/doc) Illustration of transformation actions taking into account the pivot

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"0.1.2--2025-03-25--08-09"
+  buildVersion:"0.1.2--2025-03-25--10-05"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"0.1.2--2025-03-25--08-09"
+  buildVersion:"0.1.2--2025-03-25--10-05"
 };
