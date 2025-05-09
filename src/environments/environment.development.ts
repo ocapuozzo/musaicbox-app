4d800e5e05cb4250a4b540106814fd390bfa0bbb
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-05-09 19:01:06 +0200

    (doc) add ref section Multiplication (with build)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"0.1.2--2025-05-09--18-59"
+  buildVersion:"0.1.2--2025-05-09--19-00"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"0.1.2--2025-05-09--18-59"
+  buildVersion:"0.1.2--2025-05-09--19-00"
 };
