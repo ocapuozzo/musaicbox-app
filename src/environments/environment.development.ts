266d78aa41abb93ff2902feffb814ce5b4cb79df
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2025-05-31 18:24:20 +0200

    (doc) add nowrap macro asciidoc in musaic naming TIP LT

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  buildVersion:"0.1.2--2025-05-31--16-12"
+  buildVersion:"0.1.2--2025-05-31--18-23"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  buildVersion:"0.1.2--2025-05-31--16-12"
+  buildVersion:"0.1.2--2025-05-31--18-23"
 };
